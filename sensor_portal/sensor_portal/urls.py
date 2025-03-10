268d47c67e34f72be0af809c320fc5cc1563d2a6
Marius Bølset Gisleberg <Marius.marcello@live.no>
2025-03-10 18:55:20 +0100

    added schema for swagger

--- sensor_portal/sensor_portal/urls.py
+++ sensor_portal/sensor_portal/urls.py
@@ -18,8 +18,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework_simplejwt import views as jwt_views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 from . import api
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="PAM API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
