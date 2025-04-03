bd72cb6936e676d868a640592a4e9653764a7788
Sander Ekanger <sander.ekanger@gmail.com>
2025-04-03 09:03:59 +0200

    remove icon

--- frontend_pam/src/components/Navbar/Navbar.tsx
+++ frontend_pam/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import AuthContext from '@/auth/AuthContext';
 import { Link } from '@tanstack/react-router';
 import { useContext } from 'react';
 import { FaUser } from "react-icons/fa";
-import { Menu } from 'lucide-react';
 
 type AuthContextType = {
   user: any;
@@ -17,7 +16,6 @@ function Navbar() {
       <div className="flex flex-wrap items-center justify-between w-full">
         <div className="flex items-center space-x-6 ">
           <Link to="/" className="text-white text-3xl sm:text-4xl">PAM</Link>
-          <Menu className="text-white text-3xl sm:text-4xl translate-y-0.5" />
         </div>
         <div className="flex items-center space-x-4 mt-2 sm:mt-0">
           <Link to="/" className="hidden sm:block text-white text-lg sm:text-2xl">{user.username}</Link>
