28a42bde5f6d76bafca14fb90b390d117905cd13
johannbb <johannbb@stud.ntnu.no>
2025-04-07 13:15:04 +0200

    change order on detail tabs

--- frontend_pam/src/components/pagenav/DeviceNav/DeviceNav.tsx
+++ frontend_pam/src/components/pagenav/DeviceNav/DeviceNav.tsx
@@ -7,6 +7,16 @@ function DeviceNav({ activeTab, setActiveTab }: DeviceNavProps) {
   return (
     <nav className="border-b border-gray-200">
       <ul className="flex flex-row gap-8 px-6">
+      <li>
+          <button
+            onClick={() => setActiveTab('siteDetails')}
+            className={`inline-block py-4 text-lg hover:text-gray-600 ${
+              activeTab === 'siteDetails' ? 'border-b-2 border-black' : ''
+            }`}
+          >
+            Site Details
+          </button>
+        </li>
         <li>
           <button
             onClick={() => setActiveTab('details')}
@@ -15,16 +25,6 @@ function DeviceNav({ activeTab, setActiveTab }: DeviceNavProps) {
             }`}
           >
             Device Details
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => setActiveTab('siteDetails')}
-            className={`inline-block py-4 text-lg hover:text-gray-600 ${
-              activeTab === 'siteDetails' ? 'border-b-2 border-black' : ''
-            }`}
-          >
-            Site Details
           </button>
         </li>
         <li>
