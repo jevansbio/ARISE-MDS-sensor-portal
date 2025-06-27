9e9fac0c849044be7d61235f2a0b4c1cb2046137
jevansbio <32939196+jevansbio@users.noreply.github.com>
2025-06-27 12:34:41 +0200

    Finish DSI push

--- sensor_portal/dsi/tasks.py
+++ sensor_portal/dsi/tasks.py
@@ -4,10 +4,14 @@ from sensor_portal.celery import app
 
 from .api_functions import post_data
 
+# Freek Test project 139
+# Freek Test Site 1
+# Freek Test Model 1
+
 
 @app.task(name="push_to_dsi")
 @register_job("Push to ARISE DSI", "push_to_dsi", "datafile", True,
-              default_args={"dsi_project_id": 1, "dsi_site_id": 1})
-def push_to_dsi_task(datafile_pks, dsi_project_id, dsi_site_id):
+              default_args={"dsi_project_id": 139, "dsi_site_id": 1, "dsi_sensor_model_id": 1})
+def push_to_dsi_task(datafile_pks, dsi_project_id, dsi_site_id, dsi_sensor_model_id, **kwargs):
     post_data(media_pks=datafile_pks, dsi_project_id=dsi_project_id,
-              dsi_site_id=dsi_site_id)
+              dsi_site_id=dsi_site_id, dsi_sensor_model_id=dsi_sensor_model_id)
